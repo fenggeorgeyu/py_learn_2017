ffd85bd9974337362386721505bd5127f382c79c
Feng Yu <fyu@ysu.edu>
2021-02-09 19:20:10 -0500

    [add] sqlite3 examples

---
+++ sqlite3/test_create_db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+db_file_name='test.db'
+conn=sqlite3.connect(db_file_name)
+print("opened db {} successfully".format(db_file_name))
+
+conn.execute('''CREATE TABLE COMPANY
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL,
+         ADDRESS        CHAR(50),
+         SALARY         REAL);''')
+
+print('Table created successfully')
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+
+conn.commit()
+
+print("records created successfully.")
+
+conn.close()
+
+
+
